76f411b9d9cb6f0fe8746b9f021fef1a912da6b1
MahfuzNazib <nazibmahfuz60@gmail.com>
2023-06-08 12:51:10 +0600

    Modification on _TopHeader.cshtml file & Adding Some Folder

---
+++ Src/EyeHospitalMIS/EyeHospitalMIS/Controllers/SystemInfo/BloodGroupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EyeHospitalMIS.Controllers.SystemInfo
+{
+    public class BloodGroupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Src/EyeHospitalMIS/EyeHospitalMIS/Controllers/SystemInfo/DepartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EyeHospitalMIS.Controllers.SystemInfo
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
