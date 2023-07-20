ca0605760842343bf55926bc303d207401139adf
MahfuzNazib <nazibmahfuz60@gmail.com>
2023-07-20 17:32:49 +0600

    Design Doctor Registration Page in Razor Page

---
+++ Src/EyeHospitalMIS/EyeHospitalMIS/Controllers/Doctor/DoctorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EyeHospitalMIS.Controllers.Doctor
+{
+    public class DoctorController : Controller
+    {
+        private readonly string doctorViewPath = "Views/Doctor/";
+        public IActionResult Index()
+        {
+            return View(doctorViewPath + "Index.cshtml");
+        }
+
+        public IActionResult AddDoctor()
+        {
+            return View(doctorViewPath + "Add.cshtml");
+        }
+    }
+}
