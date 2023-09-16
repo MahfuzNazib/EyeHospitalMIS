109c00b53f661cb491cde4e6923f8ab271c73388
NazibMahfuz <mahfuz@uniclienttechnologies.com>
2023-09-16 14:15:09 +0600

    Add modal as partial view

---
+++ Src/EyeHospitalMIS/EyeHospitalMIS/Controllers/SystemData/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using EysHospitalMIS.Models.SystemData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EyeHospitalMIS.Controllers.SystemInfo
+{
+    public class DepartmentController : Controller
+    {
+        private readonly string departmentViewPath = "Views/SystemData/Department/";
+        public IActionResult Index()
+        {
+            return View(departmentViewPath + "Index.cshtml");
+        }
+
+        public IActionResult AddDepartment()
+        {
+            Department department = new Department();
+            return PartialView(departmentViewPath + "_Add.cshtml", department);
+        }
+    }
+}
--- Src/EyeHospitalMIS/EyeHospitalMIS/wwwroot/js/site.js
+++ Src/EyeHospitalMIS/EyeHospitalMIS/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*Custom JS File */
 
-// Write your JavaScript code.
+//Open Ajax Model Start
+$(function () {
+    var PlaceHolderElement = $('#PlaceHolderHere');
+    $('button[data-bs-toggle="ajax-modal"]').click(function (event) {
+        console.log("Here");
+        var url = $(this).data('url');
+        var decodedUrl = decodeURIComponent(url);
+        $.get(decodedUrl).done(function (data) {
+            PlaceHolderElement.html(data);
+            PlaceHolderElement.find('.modal').modal('show');
+        })
+    })
+});
